3407e2cbc219d20e2be1363eef3fef9883314062
Raphael Ugha <raphaelugha@gmail.com>
2018-07-18 17:36:33 +0100

    Completed all tasks

--- hogwarts_lab_start/students_controller.rb
+++ hogwarts_lab_start/students_controller.rb
@@ -5,15 +5,47 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
-
-# show
+get '/students' do
+  @students = Student.all()
+  erb( :index )
+end
 
 # new
+get '/students/new' do
+  erb(:new)
+end
 
-# create
+#create
+post '/students' do
+ puts params
+ @student_new = Student.new(params)
+ @student_new.save
+ erb( :create )
+end
+
+# show
+get '/students/:id' do
+  @student = Student.find(params['id'])
+  erb( :show )
+end
 
 # edit
+get '/students/:id/edit' do
+  @houses = House.all()
+  @student = Student.find(params['id'])
+  erb( :edit)
+end
 
 # update
+post '/students/:id' do
+  student = Student.new( params )
+  student.update()
+  redirect to "students/" + params['id']
+end
 
 # destroy
+post '/students/:id/delete' do
+  student = Student.new(params)
+  student.delete()
+  redirect to '/students'
+end
